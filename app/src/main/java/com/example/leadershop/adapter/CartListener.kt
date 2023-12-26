57d5bcbc86c880b2fa764d2af5039ef740bc5dea
soufyane felate <soufyanfellat03@gmail.com>
2023-12-26 20:16:50 +0100

    Fix and update cartPage

---
+++ app/src/main/java/com/example/leadershop/adapter/CartListener.kt
@@ -0,0 +1,5 @@
+package com.example.leadershop.adapter
+
+interface CartListener {
+    fun onCartItemAdded(price: Double)
+}
---
+++ app/src/main/java/com/example/leadershop/adapter/CartManager.kt
@@ -0,0 +1,4 @@
+package com.example.leadershop.adapter
+
+class CartManager {
+}
