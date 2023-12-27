b2635a25f4c8e26252a484872b82df55b4506182
soufyane felate <soufyanfellat03@gmail.com>
2023-12-27 11:46:25 +0100

    add function dellet

---
+++ app/src/main/java/com/example/leadershop/Update_Product/update.kt
@@ -0,0 +1,12 @@
+package com.example.leadershop.Update_Product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.leadershop.R
+
+class update : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }
+}
